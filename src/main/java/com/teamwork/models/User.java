4128e470f184fbe04de5ecc0f05880822bd58966
Dangerous <342726779@qq.com>
2017-11-18 22:35:47 +0800

    add by zouwei

---
+++ src/main/java/com/teamwork/models/User.java
@@ -0,0 +1,75 @@
+package com.teamwork.models;
+
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = -3258839839160856613L;
+    private Long id;
+    private String userName;
+    private String passWord;
+    private String tel;
+    private String eMail;
+    private String gender;
+    private String avatar;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
+
