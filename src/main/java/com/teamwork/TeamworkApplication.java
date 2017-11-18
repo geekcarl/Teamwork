4bb1740b95fad9d5f42444206cafe84c68f218f8
zengxiangxin <aaazengxx@163.com>
2017-11-18 20:42:46 +0800

    init team work

---
+++ src/main/java/com/teamwork/TeamworkApplication.java
@@ -0,0 +1,12 @@
+package com.teamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamworkApplication.class, args);
+	}
+}
